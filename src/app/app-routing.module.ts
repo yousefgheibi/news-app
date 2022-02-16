eaf52148e0531e7ca4ca910bbaab7621f1f138c7
Yousef <gheibiyousef2000@gmail.com>
2022-02-16 22:56:23 +0330

    some change on style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,6 @@ const routes: Routes = [
   { path: '' , component : HomeComponent },
   { path: 'headlines' , 
     loadChildren: () => import('./headlines/headlines.module').then(m => m.HeadlinesModule) },
-  { path: 'sources',
-    loadChildren: () => import('./sources/sources.module').then(m => m.SourcesModule) },
   { path: 'favorites',
   loadChildren: () => import('./favorites/favorites.module').then(m => m.FavoritesModule) }
 ];
